0157aa78976f613787318b6dd2396188dfbabe4c
LuisMikey22 <llucatero_23@alu.uabcs.mx>
2025-05-25 01:40:58 -0700

    Diseño de consultar clientes terminado
    Ventana principal de clientes.

--- src/views/ClientView.java
+++ src/views/ClientView.java
@@ -4,19 +4,11 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-import controllers.AuthController;
-import controllers.DishController;
-import controllers.OrderController;
-import controllers.InventoryController;
-import customClasses.RoundButton;
-import customClasses.RoundPanel;
+import customClasses.*;
 
 public class ClientView {
 
 	public JFrame frame;
-	public JPanel buttonPnl;
-	public JLabel restaurantNameLbl;
-	public RoundButton foodBttn, orderBttn, clientBttn, inventoryBttn, returnBttn;
 	public JPanel mainPnl;
 	public Image image;
 	public ImageIcon imageIcon;
@@ -24,15 +16,11 @@ public class ClientView {
 	public String currentWindow;
 	public int relativeXSize;
 	public int relativeYSize;
-	public AuthController ac;
-	public DishController dc;
-	public OrderController oc;
-	public InventoryController ic;
 	
 	
 	public ClientView(String title, int frameWidth, int frameHeight) {
 		frame = new JFrame(); 
-		imageIcon = new ImageIcon("/images/elManglarLogo.png"); //icono de la ventana
+		imageIcon = new ImageIcon(ClientView.class.getResource("/images/elManglarLogo.png")); //icono de la ventana
 		frame.setIconImage(imageIcon.getImage());
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
@@ -42,189 +30,12 @@ public class ClientView {
 		frame.setMinimumSize(new Dimension(350, 350));
 		frame.setTitle(title); 
 		
-		buttonPnl = new JPanel();
-		buttonPnl.setBackground(Color.decode("#DEFFDB"));
-		buttonPnl.setLayout(new GridLayout(6, 1, 0, 10));
-		frame.add(buttonPnl, BorderLayout.WEST);
-		
-		image = new ImageIcon(ClientView.class.getResource("/images/elManglarName.png")).getImage().getScaledInstance(200, 40, Image.SCALE_SMOOTH);
-		imageIcon = new ImageIcon(image);
-		restaurantNameLbl = new JLabel(imageIcon);
-		buttonPnl.add(restaurantNameLbl);
-		
-		image = new ImageIcon("src/images/food.png").getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
-		imageIcon = new ImageIcon(image);
-		foodBttn = new RoundButton(30, imageIcon); 
-		foodBttn.setBackground(Color.decode("#244E23"));
-		foodBttn.setFont(new Font("Caladea Bold", Font.BOLD, 28)); 
-		foodBttn.setForeground(Color.white);
-		foodBttn.setText("Platillos");
-		buttonPnl.add(foodBttn);
-		
-		foodBttn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose(); //destruir ventana actual y llevar al usuario a la ventana de platillos
-				dc = new DishController("Platillos", frame.getWidth(), frame.getHeight()); //crear controlador de platillos y asignar parámetros a la ventana
-				dc.dishes(); //llamar al método que crea y muestra la ventana de platillos
-			}
-		});
-		
-		foodBttn.addMouseListener(new MouseAdapter() {
-			public void mouseEntered(MouseEvent evt) {
-				foodBttn.setBackground(Color.decode("#3C7E3A"));
-		    }
-
-		    public void mouseExited(MouseEvent evt) {
-		    	foodBttn.setBackground(Color.decode("#244E23"));
-		    }
-		});
-		
-		
-		//botón de órdenes
-		image = new ImageIcon(ClientView.class.getResource("/images/order.png")).getImage().getScaledInstance(30, 35, Image.SCALE_SMOOTH);
-		imageIcon = new ImageIcon(image);
-		orderBttn = new RoundButton(30, imageIcon); //botón
-		orderBttn.setBackground(Color.decode("#3C7E3A"));
-		orderBttn.setFont(new Font("Caladea Bold", Font.BOLD, 28)); 
-		orderBttn.setForeground(Color.white);
-		orderBttn.setText("Órdenes");
-		buttonPnl.add(orderBttn);
-		
-		//ir a ordenes
-		orderBttn.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose(); //destruir ventana actual y llevar al usuario a la ventana de ordenes
-				oc = new OrderController("Ordenes", frame.getWidth(), frame.getHeight()); //crear controlador de ordenes y asignar parámetros a la ventana
-				oc.orders(); //llamar al método que crea y muestra la ventana de ordenes
-			}
-			
-		});
-		
-		//efecto hover del botón órdenes
-		orderBttn.addMouseListener(new MouseAdapter() {
-			public void mouseEntered(MouseEvent evt) {
-				orderBttn.setBackground(Color.decode("#3C7E3A"));
-		    }
-
-		    public void mouseExited(MouseEvent evt) {
-		    	orderBttn.setBackground(Color.decode("#244E23"));
-		    }
-		});
-		
-		
-		//botón de clientes
-		image = new ImageIcon(ClientView.class.getResource("/images/client.png")).getImage().getScaledInstance(30, 35, Image.SCALE_SMOOTH);
-		imageIcon = new ImageIcon(image);
-		clientBttn = new RoundButton(30, imageIcon); //botón
-		clientBttn.setBackground(Color.decode("#3C7E3A"));
-		clientBttn.setFont(new Font("Caladea Bold", Font.BOLD, 28)); 
-		clientBttn.setForeground(Color.white);
-		clientBttn.setText("Clientes");
-		buttonPnl.add(clientBttn);
-		
-		
-		//botón de inventario
-		image = new ImageIcon(ClientView.class.getResource("/images/inventory.png")).getImage().getScaledInstance(30, 28, Image.SCALE_SMOOTH);
-		imageIcon = new ImageIcon(image);
-		inventoryBttn = new RoundButton(30, imageIcon); //botón
-		inventoryBttn.setBackground(Color.decode("#244E23"));
-		inventoryBttn.setFont(new Font("Caladea Bold", Font.BOLD, 28)); 
-		inventoryBttn.setForeground(Color.white);
-		inventoryBttn.setText("Inventario");
-		buttonPnl.add(inventoryBttn);
-		
-		//ir a inventario
-		inventoryBttn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose(); //destruir ventana actual y llevar al usuario a la ventana de inventario
-				ic = new InventoryController("Inventario", frame.getWidth(), frame.getHeight()); //crear controlador de inventario y asignar parámetros a la ventana
-				ic.inventory(); //llamar al método que crea y muestra la ventana de inventario
-			}
-		});
-		
-		//efecto hover del botón inventario
-		inventoryBttn.addMouseListener(new MouseAdapter() {
-			public void mouseEntered(MouseEvent evt) {
-				inventoryBttn.setBackground(Color.decode("#3C7E3A"));
-		    }
-
-		    public void mouseExited(MouseEvent evt) {
-		    	inventoryBttn.setBackground(Color.decode("#244E23"));
-		    }
-		});
-		
-		
-		//botón de salir
-		image = new ImageIcon(ClientView.class.getResource("/images/return.png")).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
-		imageIcon = new ImageIcon(image);
-		returnBttn = new RoundButton(30, imageIcon); //botón
-		returnBttn.setBackground(Color.decode("#EF2D2D"));
-		returnBttn.setFont(new Font("Caladea Bold", Font.BOLD, 28)); 
-		returnBttn.setForeground(Color.white);
-		returnBttn.setText("Salir");
-		buttonPnl.add(returnBttn);
-		
-		//volver a la ventana anterior de acorde a la ventana actual
-		returnBttn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				switch(currentWindow) { 
-					case "clients":
-						frame.dispose(); //destruir ventana actual y llevar al usuario a la ventana de login
-						ac = new AuthController("Login", frame.getWidth(), frame.getHeight()); //crear controlador de login y asignar parámetros a la ventana
-						ac.login(); //llamar al método que crea y muestra la ventana de login
-					break;
-				
-					case "newClient":
-						frame.remove(mainPnl);
-						clients();
-					break;
-					
-					case "editClient":
-						frame.remove(mainPnl);
-						clients();
-					break;
-					
-					case "clientHistory":
-						frame.remove(mainPnl);
-						clients();
-					break;
-				}
-				
-				frame.repaint();
-				frame.revalidate();
-			}
-		});
-		
-		//efecto hover del botón volver
-		returnBttn.addMouseListener(new MouseAdapter() {
-			public void mouseEntered(MouseEvent evt) {
-				if(currentWindow.equals("clients")) {
-					returnBttn.setBackground(Color.decode("#ED5C5C"));
-				}else {
-					returnBttn.setBackground(Color.decode("#3C7E3A"));
-				}
-		    }
-
-		    public void mouseExited(MouseEvent evt) {
-		    	if(currentWindow.equals("clients")) {
-					returnBttn.setBackground(Color.decode("#EF2D2D"));
-				}else {
-					returnBttn.setBackground(Color.decode("#244E23"));
-				}
-		    }
-		});
+		SideBarPanel sideBar = new SideBarPanel(frame);
+		JPanel buttonPanel = sideBar.createSidePanel();
+		frame.add(buttonPanel, BorderLayout.WEST);
 	}
 	
 	public void clients() {
-		currentWindow = "clients"; 
-		returnBttn.setBackground(Color.decode("#EF2D2D")); 
-		returnBttn.setText("Salir");
-		
 		mainPnl = new JPanel();
 		mainPnl.setBackground(Color.white);
 		mainPnl.setBorder(BorderFactory.createEmptyBorder(30, 45, 30, 45)); 
@@ -244,7 +55,8 @@ public class ClientView {
 		headerPnl.add(clientsLbl);
 		
 		JPanel actionPnl = new JPanel();
-		actionPnl.setLayout(new FlowLayout(FlowLayout.TRAILING, 30, 0));
+		actionPnl.setLayout(new GridLayout(1, 4, 20, 0));
+		//actionPnl.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 		actionPnl.setOpaque(false); 
 		headerPnl.add(actionPnl);
 		
@@ -267,7 +79,7 @@ public class ClientView {
 		searchTxtFld.setForeground(Color.decode("#999999")); 
 		searchTxtFld.setOpaque(false);
 		searchBarPnl.add(searchTxtFld,  BorderLayout.CENTER);
-				
+		
 		RoundButton searchBttn = new RoundButton(30);
 		searchBttn.setBackground(Color.decode("#244E23"));
 		searchBttn.setFont(new Font("Caladea Bold", Font.BOLD, 20));
@@ -285,11 +97,13 @@ public class ClientView {
 		    }
 		});
 				
+		actionPnl.add(Box.createHorizontalStrut(40));
+		
 		RoundButton newClient = new RoundButton(30);
 		newClient.setBackground(Color.decode("#2EA623"));
 		newClient.setFont(new Font("Caladea Bold", Font.BOLD, 20));
 		newClient.setForeground(Color.white);
-		newClient.setText("Añadir nuevo cliente");
+		newClient.setText("Añadir");
 		actionPnl.add(newClient); 
 
 		newClient.addActionListener(new ActionListener() {
@@ -311,58 +125,66 @@ public class ClientView {
 		    	newClient.setBackground(Color.decode("#2EA623"));
 		    }
 		});
+
 		
 		JPanel clientsPnl = new JPanel();
-		clientsPnl.setBorder(BorderFactory.createEmptyBorder(40, 0, 40, 0));  
-		clientsPnl.setLayout(new FlowLayout());	
-		clientsPnl.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		clientsPnl.setBorder(BorderFactory.createEmptyBorder(40, 0, 10, 0));  
+		clientsPnl.setLayout(new BorderLayout());	
 		clientsPnl.setOpaque(false);
+		//clientsPnl.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 		mainPnl.add(clientsPnl, BorderLayout.CENTER);
 		
-		RoundButton editBttn = new RoundButton(30);
-		editBttn.setBackground(Color.decode("#C07A00"));
-		editBttn.setFont(new Font("Caladea Bold", Font.BOLD, 20));
-		editBttn.setForeground(Color.white);
-		editBttn.setText("Editar");
-		clientsPnl.add(editBttn); 
+		String[] column =  {"Nombre", "Dirección", "Número", "Correo", "Acción"};
+		Object[][] data = {{"José Eduardo Guereque", "Del Árbol 169, col. La fuente", "6128682392", "Gue123@gmail.com", null},
+						   {"Ángel Gabriel Mendoza", "Del Árbol 169, col. La fuente", "6151093321", "litrin@gmail.com", null},
+						   {"Christian Ivan Rivera", "Del Árbol 169, col. La fuente", "6121761317", "civan@gmail.com", null}, 
+						   {"Luis Miguel Pérez", "Del Árbol 169, col. La fuente", "6122170991", "lucatero@gmail.com", null}};
 		
-		editBttn.addActionListener(new ActionListener() {
+		/*DefaultTableModel tableModel = new DefaultTableModel(data, column) {
+			private static final long serialVersionUID = 1L;
+
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				frame.remove(mainPnl);
-				editClient();
-				frame.repaint();
-				frame.revalidate();
-			}
-		});
+		    public boolean isCellEditable(int row, int column) {
+		       return false;
+		    }
+			
+			
+		};*/
 		
+		JTable clientsTable = new JTable(data, column);
+		clientsTable.getColumn("Acción").setCellRenderer(new PanelRenderer());
+		clientsTable.setFont(new Font("Caladea Bold", Font.BOLD, 15));
+		clientsTable.setDefaultEditor(Object.class, null);
+		clientsTable.setRowHeight(40);
+		clientsTable.setShowGrid(false);
+		clientsTable.setShowHorizontalLines(false);
+		clientsTable.setShowVerticalLines(false);
 		
-		RoundButton historyBttn = new RoundButton(30);
-		historyBttn.setBackground(Color.decode("#2EA623"));
-		historyBttn.setFont(new Font("Caladea Bold", Font.BOLD, 20));
-		historyBttn.setForeground(Color.white);
-		historyBttn.setText("Historial");
-		clientsPnl.add(historyBttn);
+		clientsTable.getTableHeader().setBackground(Color.decode("#555BF6"));
+		clientsTable.getTableHeader().setForeground(Color.white);
+		clientsTable.getTableHeader().setFont(new Font("Caladea Bold", Font.BOLD, 15));
+		clientsTable.getTableHeader().setReorderingAllowed(false);
+		clientsTable.getTableHeader().setResizingAllowed(false);
 		
-		historyBttn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frame.remove(mainPnl);
-				clientHistory();
-				frame.repaint();
-				frame.revalidate();
-			}
-		});
+		clientsTable.setBackground(Color.white);
+		clientsTable.setForeground(Color.decode("#244E23"));
+		clientsTable.setSelectionBackground(Color.white);
+		clientsTable.setSelectionForeground(Color.decode("#2EA623"));
+		clientsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+				
+		JScrollPane scrollPane = new JScrollPane(clientsTable);
+		scrollPane.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setOpaque(false);
+	    scrollPane.getViewport().setOpaque(false);
+		clientsPnl.add(scrollPane, BorderLayout.CENTER);
 	
 		frame.setVisible(true);
 	}
 	
 	
 	public void newClient() {
-		currentWindow = "newClient"; 
-		returnBttn.setBackground(Color.decode("#244E23"));
-		returnBttn.setText("Volver");
-		
 		mainPnl = new JPanel();
 		mainPnl.setBackground(Color.white);
 		mainPnl.setBorder(BorderFactory.createEmptyBorder(30, 45, 30, 45)); 
@@ -441,10 +263,6 @@ public class ClientView {
 	
 	
 	public void editClient() {
-		currentWindow = "editClient"; 
-		returnBttn.setBackground(Color.decode("#244E23")); 
-		returnBttn.setText("Volver");
-		
 		mainPnl = new JPanel();
 		mainPnl.setBackground(Color.white);
 		mainPnl.setBorder(BorderFactory.createEmptyBorder(30, 45, 30, 45)); 
@@ -523,10 +341,6 @@ public class ClientView {
 	
 	
 	public void clientHistory() {
-		currentWindow = "clientHistory"; 
-		returnBttn.setBackground(Color.decode("#244E23")); 
-		returnBttn.setText("Volver");
-		
 		mainPnl = new JPanel();
 		mainPnl.setBackground(Color.white);
 		mainPnl.setBorder(BorderFactory.createEmptyBorder(30, 45, 30, 45)); 
