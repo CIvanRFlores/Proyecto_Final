c861957b4835c4333cc5245091079171b55958fc
LuisMikey22 <198980268+LuisMikey22@users.noreply.github.com>
2025-05-27 00:09:20 -0700

    Diseño de historial de cliente terminado

--- src/views/ClientView.java
+++ src/views/ClientView.java
@@ -174,6 +174,7 @@ public class ClientView {
             public void onView(int row) {
                 System.out.println("Viewed row : " + (row+1));
                 frame.remove(mainPnl);
+                clientHistory();
 				frame.repaint();
 				frame.revalidate();
             }
@@ -323,4 +324,101 @@ public class ClientView {
 		frame.setVisible(true);
 	}
 	
+	public void clientHistory() {
+		mainPnl = new JPanel();
+		mainPnl.setBackground(Color.white);
+		mainPnl.setBorder(BorderFactory.createEmptyBorder(30, 45, 30, 45)); 
+		mainPnl.setLayout(new BorderLayout());
+		frame.add(mainPnl, BorderLayout.CENTER);
+		
+		JPanel headerPnl = new JPanel();
+		headerPnl.setLayout(new GridLayout(2, 1, 0, 15));
+		headerPnl.setOpaque(false); 
+		mainPnl.add(headerPnl, BorderLayout.NORTH);
+		
+		JLabel clientsLbl = new JLabel("Consultar historial de compras");
+		clientsLbl.setFont(new Font("Caladea Bold", Font.BOLD, 36));
+		clientsLbl.setForeground(Color.decode("#244E23"));
+		clientsLbl.setHorizontalAlignment(JLabel.LEFT); 
+		clientsLbl.setHorizontalAlignment(SwingConstants.LEFT); 
+		headerPnl.add(clientsLbl);
+				
+		JPanel actionPnl = new JPanel();
+		actionPnl.setLayout(new GridLayout(1, 4, 20, 0));
+		actionPnl.setOpaque(false); 
+		headerPnl.add(actionPnl);
+		
+		actionPnl.add(Box.createHorizontalStrut(0));
+		actionPnl.add(Box.createHorizontalStrut(0));
+		actionPnl.add(Box.createHorizontalStrut(0));
+		
+		RoundButton downloadBttn = new RoundButton(30);
+		downloadBttn.setBackground(Color.decode("#367181"));
+		downloadBttn.setFont(new Font("Caladea Bold", Font.BOLD, 20));
+		downloadBttn.setForeground(Color.white);
+		downloadBttn.setText("Descargar");
+		actionPnl.add(downloadBttn); 
+
+		downloadBttn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.remove(mainPnl);
+				clients();
+				frame.repaint();
+				frame.revalidate();
+			}
+		});
+		
+		downloadBttn.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent evt) {
+				downloadBttn.setBackground(Color.decode("#264F59"));
+		    }
+
+		    public void mouseExited(MouseEvent evt) {
+		    	downloadBttn.setBackground(Color.decode("#367181"));
+		    }
+		});
+		
+		JPanel clientsPnl = new JPanel();
+		clientsPnl.setBorder(BorderFactory.createEmptyBorder(40, 0, 10, 0));  
+		clientsPnl.setLayout(new BorderLayout());	
+		clientsPnl.setOpaque(false);
+		mainPnl.add(clientsPnl, BorderLayout.CENTER);
+		
+		String[] column =  {"Fecha", "Dirección", "Monto", "Órden"};
+		
+		Object[][] data = {{"27/05/25", "Del Árbol 169, col. La fuente", "$15.00", "Cacahuates enchilados"}};
+		
+		DefaultTableModel tableModel = new DefaultTableModel(data, column);
+		JTable clientsTable = new JTable(tableModel);
+		
+		clientsTable.setFont(new Font("Caladea Bold", Font.BOLD, 16));
+		clientsTable.setDefaultEditor(Object.class, null);
+		clientsTable.setRowHeight(40);
+		clientsTable.setShowGrid(false);
+		clientsTable.setShowHorizontalLines(true);
+		clientsTable.setShowVerticalLines(false);
+		
+		clientsTable.getTableHeader().setBackground(Color.decode("#555BF6"));
+		clientsTable.getTableHeader().setForeground(Color.white);
+		clientsTable.getTableHeader().setFont(new Font("Caladea Bold", Font.BOLD, 16));
+		clientsTable.getTableHeader().setReorderingAllowed(false);
+		clientsTable.getTableHeader().setResizingAllowed(false);
+		
+		clientsTable.setBackground(Color.white);
+		clientsTable.setForeground(Color.decode("#244E23"));
+		clientsTable.setSelectionBackground(Color.white);
+		clientsTable.setSelectionForeground(Color.decode("#2EA623"));
+		clientsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+				
+		JScrollPane scrollPane = new JScrollPane(clientsTable);
+		scrollPane.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setOpaque(false);
+	    scrollPane.getViewport().setOpaque(false);
+		clientsPnl.add(scrollPane, BorderLayout.CENTER);
+	
+		frame.setVisible(true);
+	}
 }
