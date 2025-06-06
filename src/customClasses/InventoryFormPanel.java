13aa152dc98ec468b0afd378d15a9e454019a326
LuisMikey22 <198980268+LuisMikey22@users.noreply.github.com>
2025-06-05 23:49:58 -0700

    Clase de formulario de inventario

---
+++ src/customClasses/InventoryFormPanel.java
@@ -0,0 +1,211 @@
+package customClasses;
+
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class InventoryFormPanel {
+	
+	JFrame frame;
+	JTextField nameTxtFld;
+	JTextField codeTxtFld;
+	JTextField quantityTxtFld;
+	
+	ImageIcon imageIcon;
+	Image image;
+	int relativeXSize;
+	Font font;
+	
+	public InventoryFormPanel(JFrame frame) {
+		this.frame = frame;
+	}
+	
+	public JPanel createInventoryForm() {
+		JPanel inventoryPnl = new JPanel();
+		inventoryPnl.setBorder(BorderFactory.createEmptyBorder(20, 0, 40, 0)); 
+		inventoryPnl.setLayout(new GridLayout(6, 2, 30, 30));	
+		inventoryPnl.setOpaque(false); 
+		
+		
+		JLabel nameLbl = new JLabel("Nombre del inventario:");
+		nameLbl.setForeground(Color.decode("#244E23")); 
+		nameLbl.setFont(new Font("Caladea Bold", Font.BOLD, 20));
+		nameLbl.setHorizontalAlignment(SwingConstants.LEFT); 
+		nameLbl.setVerticalAlignment(SwingConstants.BOTTOM); 
+		nameLbl.setOpaque(false);
+		inventoryPnl.add(nameLbl);
+		
+		JLabel codeLbl = new JLabel("Código de inventario:");
+		codeLbl.setForeground(Color.decode("#244E23")); 
+		codeLbl.setFont(new Font("Caladea Bold", Font.BOLD, 20));
+		codeLbl.setHorizontalAlignment(SwingConstants.LEFT); 
+		codeLbl.setVerticalAlignment(SwingConstants.BOTTOM); 
+		codeLbl.setOpaque(false);
+		inventoryPnl.add(codeLbl);
+		
+		RoundPanel nameTxtFldPnl = new RoundPanel(30);  
+		nameTxtFldPnl.setBackground(Color.decode("#EDEDED"));
+		nameTxtFldPnl.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));  
+		nameTxtFldPnl.setForeground(Color.decode("#244E23")); 
+		nameTxtFldPnl.setLayout(new BorderLayout());
+		inventoryPnl.add(nameTxtFldPnl);
+		
+		nameTxtFld = new JTextField("");
+		nameTxtFld.setBorder(null);
+		nameTxtFld.setFont(new Font("Caladea Bold", Font.BOLD, 20)); 
+		nameTxtFld.setForeground(Color.decode("#244E23")); 
+		nameTxtFld.setOpaque(false);
+		nameTxtFldPnl.add(nameTxtFld, BorderLayout.CENTER);
+		
+		nameTxtFld.addKeyListener((KeyListener) new KeyAdapter() {
+		    @Override
+		    public void keyTyped(KeyEvent e) {
+		        char c = e.getKeyChar();
+		        if (!Character.isLetter(c) && !Character.isWhitespace(c)) {
+		            e.consume();
+		    }
+		}});
+		
+		
+		RoundPanel codeTxtFldPnl = new RoundPanel(30);  
+		codeTxtFldPnl.setBackground(Color.decode("#EDEDED"));
+		codeTxtFldPnl.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));  
+		codeTxtFldPnl.setForeground(Color.decode("#244E23")); 
+		codeTxtFldPnl.setLayout(new BorderLayout());
+		inventoryPnl.add(codeTxtFldPnl);
+		
+		codeTxtFld = new JTextField("");
+		codeTxtFld.setBorder(null);
+		codeTxtFld.setFont(new Font("Caladea Bold", Font.BOLD, 20)); 
+		codeTxtFld.setForeground(Color.decode("#244E23")); 
+		codeTxtFld.setOpaque(false);
+		codeTxtFldPnl.add(codeTxtFld, BorderLayout.CENTER);
+		
+		codeTxtFld.addKeyListener((KeyListener) new KeyAdapter() {
+		    @Override
+		    public void keyTyped(KeyEvent e) {
+		        char c = e.getKeyChar();
+		        if (!Character.isLetter(c) && !Character.isDigit(c)) {
+		            e.consume();
+		    }
+		}});
+		
+		
+		JLabel quantityLbl = new JLabel("Cantidad:");
+		quantityLbl.setForeground(Color.decode("#244E23")); 
+		quantityLbl.setFont(new Font("Caladea Bold", Font.BOLD, 20));
+		quantityLbl.setHorizontalAlignment(SwingConstants.LEFT); 
+		quantityLbl.setVerticalAlignment(SwingConstants.BOTTOM); 
+		quantityLbl.setOpaque(false);
+		inventoryPnl.add(quantityLbl);
+		
+		inventoryPnl.add(Box.createHorizontalStrut(0));
+		
+		
+		RoundPanel quantityTxtFldPnl = new RoundPanel(30);  
+		quantityTxtFldPnl.setBackground(Color.decode("#EDEDED"));
+		quantityTxtFldPnl.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));  
+		quantityTxtFldPnl.setForeground(Color.decode("#244E23")); 
+		quantityTxtFldPnl.setLayout(new BorderLayout());
+		inventoryPnl.add(quantityTxtFldPnl);
+		
+		quantityTxtFld = new JTextField("");
+		quantityTxtFld.setBorder(null);
+		quantityTxtFld.setFont(new Font("Caladea Bold", Font.BOLD, 20)); 
+		quantityTxtFld.setForeground(Color.decode("#244E23")); 
+		quantityTxtFld.setOpaque(false);
+		quantityTxtFldPnl.add(codeTxtFld, BorderLayout.CENTER);
+		
+		quantityTxtFld.addKeyListener((KeyListener) new KeyAdapter() {
+		    @Override
+		    public void keyTyped(KeyEvent e) {
+		        char c = e.getKeyChar();
+		        if (!Character.isDigit(c)) {
+		            e.consume();
+		    }
+		}});
+		
+		inventoryPnl.add(Box.createHorizontalStrut(0));
+		
+		inventoryPnl.add(Box.createHorizontalStrut(0));
+		inventoryPnl.add(Box.createHorizontalStrut(0));
+		inventoryPnl.add(Box.createHorizontalStrut(0));
+		inventoryPnl.add(Box.createHorizontalStrut(0));
+		
+		/**cuando la ventana es redimensionada, los elementos dentro de ella cambian de tamaño**/
+		frame.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent e) {
+            	relativeXSize = (int) (frame.getHeight()*0.02);
+            	font = new Font("Caladea Bold", Font.BOLD, relativeXSize);
+       			nameLbl.setFont(font);
+       			codeLbl.setFont(font);
+       			quantityLbl.setFont(font);
+       			nameTxtFld.setFont(font);
+       			codeTxtFld.setFont(font);
+       			quantityTxtFld.setFont(font);
+       			
+       			relativeXSize = (int) (frame.getHeight()*0.01);
+       			Border border = BorderFactory.createEmptyBorder(relativeXSize, relativeXSize, relativeXSize, relativeXSize);
+       			nameTxtFldPnl.setBorder(border);  
+       			codeTxtFldPnl.setBorder(border);  
+       			quantityTxtFldPnl.setBorder(border); 
+       			
+       			frame.repaint();
+            }
+        });
+		
+		return inventoryPnl;
+	}
+	
+	
+	public boolean invFormEmptyFields() {
+		String name = getNameTxtFld(); 
+		String code = getCodeTxtFld();
+		String quantity = getQuantityTxtFld(); 
+		
+		if(name.equals("") || code.equals("") || quantity.equals("")) {
+			image = new ImageIcon(ClientFormPanel.class.getResource("/images/warning.png")).getImage().getScaledInstance(45, 45, Image.SCALE_SMOOTH); 
+			imageIcon = new ImageIcon(image);
+				
+			String message = "Complete los campos para guardar la información.";
+			JOptionPane.showMessageDialog(null, message, "Campos vacíos", JOptionPane.INFORMATION_MESSAGE, imageIcon);
+			
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
+	public String getNameTxtFld() {
+		return nameTxtFld.getText();
+	}
+	
+	public String getCodeTxtFld() {
+		return codeTxtFld.getText();
+	}
+	
+	public String getQuantityTxtFld() {
+		return quantityTxtFld.getText();
+	}
+	
+	
+	public void setNameTxtFld(String name) {
+		this.nameTxtFld.setText(name);
+	}
+	
+	public void setCodeTxtFld(String code) {
+		this.codeTxtFld.setText(code);
+	}
+	
+	public void setquantityTxtFld(int quantity) {
+		this.quantityTxtFld.setText(Integer.toString(quantity));
+	}
+	
+}
